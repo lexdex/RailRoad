7d2ec9998329de26d631a942d96546e39e7c9e92
Lex <alexdex.vip@gmail.com>
2019-02-13 04:32:44 +0200

    0.0.2

--- src/main/java/com/example/demo/domain/Route.java
+++ src/main/java/com/example/demo/domain/Route.java
@@ -1,75 +1,26 @@
 package com.example.demo.domain;
 
-import lombok.*;
-
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Route {
 
-        private  int TrainID;
-        private  String Dest_From;
-        private String Dest_To;
-        private long  unix_time;
-        private Date  time;
-
-        private ArrayList<String> nodeStation;
-
+private int Id;
+private String name;
+private RouteNode Start;
+private RouteNode FInish;
+private Train train;
+private ArrayList<RouteNode> routeNode;
 
 
-    public int getTrainID() {
-        return TrainID;
+
+    public Route(int id, String name, RouteNode start, RouteNode FInish, Train train, int length) {
+        Id = id;
+        this.name = name;
+        Start = start;
+        this.FInish = FInish;
+        this.train = train;
+        ArrayList<RouteNode> routeNode= new ArrayList<>(length);
     }
-
-    public void setTrainID(int trainID) {
-        TrainID = trainID;
-    }
-
-    public String getDest_From() {
-        return Dest_From;
-    }
-
-    public void setDest_From(String dest_From) {
-        Dest_From = dest_From;
-    }
-
-    public String getDest_To() {
-        return Dest_To;
-    }
-
-    public void setDest_To(String dest_To) {
-        Dest_To = dest_To;
-    }
-
-    public long getUnix_time() {
-        return unix_time;
-    }
-
-    public void setUnix_time(long unix_time) {
-        this.unix_time = unix_time;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
-
-
---
+++ src/main/java/com/example/demo/domain/Train.java
@@ -0,0 +1,82 @@
+package com.example.demo.domain;
+
+import java.util.ArrayList;
+
+public class Train {
+    private int Id;
+    private String Name;
+    private ArrayList<Carriage> carriages;
+
+
+    public Train(int id, String name, int CarriagesNumber) {
+        Id = id;
+        Name = name;
+        ArrayList<Carriage> carriages= new ArrayList<>(CarriagesNumber);
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+
+
+    public int TrainBookPlace(int carriageIndex,int placeIndex) {
+        if  (carriages.size()> carriageIndex)
+        {
+            carriages.get(carriageIndex).BookPlace(placeIndex);
+            return 0;
+        }
+        else{return -1;}
+
+    }
+
+    public int TrainReverseBookPlace(int carriageIndex,int placeIndex) {
+        if  (carriages.size()> carriageIndex)
+        {
+            carriages.get(carriageIndex).ReverseBookPlace(placeIndex);
+            return 0;
+        }
+        else{return -1;}
+
+    }
+
+    public int CountTrainFreePlaces (byte[] booked) {
+        int count = 0;
+        for (Carriage car : carriages) {
+           count+=car.CountFreePlaces();
+        }
+        return count;
+    }
+
+    public void showTrainBooking() {
+        for (Carriage car: carriages) {
+            int i = 0;
+            System.out.println("carriage number "+i);
+            car.showBooking();
+            i++;
+        }
+    }
+
+
+
+    void addToList(ArrayList<Carriage> carriage) {
+        carriages.addAll(carriage);
+    }
+
+
+
+
+
+}
